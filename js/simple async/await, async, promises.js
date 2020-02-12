3bc66eacd915373bec6f909567533c7d8fa82494
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-12 07:41:00 -0500

    Create await, async, promises.js

---
+++ js/simple async/await, async, promises.js
@@ -0,0 +1,36 @@
+/* Async/Await are powerful functions.
+This is a basic demonstration of
+what these are.
+
+You should probably read up on promises.
+
+But, in a nutshell, promises are used for
+getting the state of anything that is
+attached to a callback function. They
+are very useful. */
+prompt("Enter a number.").then(async p => { // this takes a prompt, which returns the user input, and promisifies it
+  var x = p*(Math.floor(Math.random()*13)+2); // returns a value that is the prompt times a random number between 2 and 13.
+  await window.alert("The number returned is: " + p); // await pauses the async function until it is fulfilled
+}
+/* Promises are mainly useful for when you need to
+have some code that should be caught at any time,
+and also code that needs to wait for something before 
+anything else happens. */
+function resolve() {
+  return new Promise(re => { // creates a new Promise object
+    re("This is a promise that has been resolved.");
+  });
+}
+async function run() { // creates a new async function
+  try {
+    let resolveFunction = await resolve(); // waits for it to be resolved
+    console.log(resolveFunction); // logs the resolve.
+    const y = 32;
+    y = 1; // creates a constant, and tries to change it.
+  } catch (err) { // catches the error...
+    await console.error(err); // and shows the error, in this case we tried to change a constant, or the Promise resolution failed
+  }
+}
+/* This is a very basic demonstration of
+Promises, async, and await. Check the
+MDN page on it for a better understanding. */
