b87d78a6b8b6b786fe83e5b248393c55e8f3b6b0
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-13 10:01:47 -0500

    formatting

--- js/NaN and Infinity.js
+++ js/NaN and Infinity.js
@@ -1,15 +1,30 @@
-/* NaN is pretty interesting. Here's a demonstration. */
+/* NaN is pretty interesting.
+Here's a demonstration. */
+
 eval(NaN + 3); // this returns NaN, same with any other operation
+
 eval(NaN + "34"); // also returns NaN
+
 /* Now the interesting thing... */
+
 console.log(typeof NaN); // this returns a number!
+
 /* Now, for ways to get NaN... */
+
 eval("34" + 120);
+
 eval("hello" / 13);
+
 /* Interestingly, when multiplying, subtracting, or dividing, JS tries to convert strings to numbers, but not with adding... */
+
 eval("100" - 52) // returns 48
+
 eval("15" + 9) // returns NaN
+
 /* Infinity is also pretty interesting. */
+
 eval(9182392787238293231 * 909237223384956621244293) // this is outside of the normal range of numbers. returns Infinity
+
 eval(23/0) // also returns Infinity, same with any division by 0.
+
 console.log(typeof Infinity) // also returns a number!
