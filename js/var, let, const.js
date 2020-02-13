5396794292aa173aac693eb445cbcb18b2cc7279
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-13 10:10:19 -0500

    Update var, let, const.js

--- js/var, let, const.js
+++ js/var, let, const.js
@@ -7,16 +7,16 @@ const z = 3; // let's declare these three variables differently.
 
 var x = 4; // you can freely re-declare variables with var in its scope.
 let y = 3; // this returns an error, you can't redeclare let variables in the same scope.
-const z = 2; // you can't change consts.
+const z = 2; // you can't change consts. returns an error
 
 /* Now, a different scope... */
 if(true) {
   console.log(x); // returns 4
-  console.log(y); // returns undefined, as variables made with let are only available in that scope
+  console.log(y); // returns an error, as variables made with let are only available in that scope
   console.log(z) // returns 3
   
   let n = 23; // how about we declare this variable with let...
 }
-console.log(n) // this returns undefined, because n isn't in the same scope!
+console.log(n) // this returns an error, because n isn't in the same scope!
 
 /* If you're reading this, hopefully this helps. */
