176a8048a566bc22afd13384835bf801bdab5a90
sperg <58081117+spergmoment@users.noreply.github.com>
2020-02-18 12:30:40 -0500

    logging, fixes

--- js/NaN and Infinity.js
+++ js/NaN and Infinity.js
@@ -1,30 +1,29 @@
 /* NaN is pretty interesting.
 Here's a demonstration. */
 
-eval(NaN + 3); // this returns NaN, same with any other operation
+console.log(NaN + 3); // this returns NaN, same with any other operation
 
-eval(NaN + "34"); // also returns NaN
+console.log(NaN - "34"); // also returns NaN
 
 /* Now the interesting thing... */
 
 console.log(typeof NaN); // this returns a number!
 
-/* Now, for ways to get NaN... */
+/* Now, for the way to get NaN... */
 
-eval("34" + 120);
+console.log("hello" / 13);
 
-eval("hello" / 13);
+/* Interestingly, when multiplying, subtracting, or dividing, JS tries to convert strings to numbers,
+but with adding, it just puts the string and number together... */
 
-/* Interestingly, when multiplying, subtracting, or dividing, JS tries to convert strings to numbers, but not with adding... */
+console.log("100" - 52) // returns 48
 
-eval("100" - 52) // returns 48
-
-eval("15" + 9) // returns NaN
+console.log("15" + 9) // returns 159
 
 /* Infinity is also pretty interesting. */
 
-eval(9182392787238293231 * 909237223384956621244293) // this is outside of the normal range of numbers. returns Infinity
+console.log(9e999 * 9e999) // this is outside of the normal range of numbers. returns Infinity
 
-eval(23/0) // also returns Infinity, same with any division by 0.
+console.log(23/0) // also returns Infinity, same with any division by 0.
 
 console.log(typeof Infinity) // also returns a number!
