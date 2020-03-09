6bc3af8ae0b54c270ab1a28e8168e0858645db88
sperg <58081117+spergmoment@users.noreply.github.com>
2020-03-09 10:56:27 -0400

    Update simple calc.js

--- js/simple calc.js
+++ js/simple calc.js
@@ -1,13 +1,9 @@
-prompt("Please enter an equation. Note: do ** for powers and exponents.")
-    .then(async p => {
-        try {
-            let e = await eval(p);
-            alert(e);
-        } catch {
-            alert("This appears to be invalid. Please try again");
-        }
-    });
-/* Don't use eval in any public level of code, 
-especially on servers, 
-as people can use it maliciously, 
-since eval can execute any js code in there. */
+let p = prompt("Please enter an equation. Note: do ** for powers and exponents.");
+try {
+    let e = Function('"use strict";return (' + p + ')')();
+    window.alert(e);
+} catch (e) {
+    window.alert(e);
+}
+/* Using eval functions like this is NOT
+a good idea in public code. Be careful. */
